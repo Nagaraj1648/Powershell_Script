aeae800eb781e3c3f4cdb6f7c216c07fefb7bd9c
Nagaraj1648 <nagarajan.k.fo@outlook.com>
2024-11-30 08:06:14 +0530

    Only Active AD Users change applied on Dev Branch

--- Active AD Users.ps1
+++ Active AD Users.ps1
@@ -2,3 +2,4 @@
 
 $activeuser = Get-ADUser -Filter { enabled -eq $true} -Properties name,Samaccountname,Whencreated | Select Samaccountname,Whencreated,name
 $activeuser.name.count
+$activeuser.Samaccountname
